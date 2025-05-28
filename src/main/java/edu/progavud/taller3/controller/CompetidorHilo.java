1a2eaebc610bbd9c6cffbc74e1fb7d871486d606
Jorge <jm2006900@gmail.com>
2025-05-27 23:17:25 -0500

    Boton salir y nueva carrera ajustados correctamente

--- src/main/java/edu/progavud/taller3/controller/CompetidorHilo.java
+++ src/main/java/edu/progavud/taller3/controller/CompetidorHilo.java
@@ -6,6 +6,7 @@ package edu.progavud.taller3.controller;
 
 import edu.progavud.taller3.model.Competidor;
 import java.util.Random;
+import java.util.concurrent.locks.LockSupport;
 
 /**
  *
@@ -15,43 +16,51 @@ public class CompetidorHilo extends Thread {
 
     private Competidor competidor;
     private ControlCarrera cCarrera;
+    private boolean isPausado;
     private Random rng;
 
-    public CompetidorHilo(ControlCarrera cCarrera, String nombre) {
+    public CompetidorHilo(ControlCarrera cCarrera, Competidor competidor) {
         this.cCarrera = cCarrera;
-        competidor = new Competidor(nombre);
+        this.competidor = competidor;
         rng = new Random();
+        isPausado = false;
     }
 
     @Override
     public void run() {
-        competidor.reiniciarPosicion();
-        long tiempoInicio = System.currentTimeMillis();
-        while (competidor.getPosicion() < cCarrera.getDistanciaCarrera()) {
-            competidor.mover(1);
-            cCarrera.moverCompetidorLabel(this, 1);
-            try {
-                sleep(rng.nextInt(0, 250));
-            } catch (InterruptedException ex) {
-                System.out.println("Soy el metodo run, algo paso");
+            competidor.reiniciarPosicion();
+            long tiempoInicio = System.currentTimeMillis();
+            while (competidor.getPosicion() < cCarrera.getDistanciaCarrera()) {
+                if(isPausado) {
+                    LockSupport.park();
+                }
+                competidor.mover(1);
+                cCarrera.moverCompetidorLabel(this, 1);
+                try {
+                    sleep(rng.nextInt(0, 250));
+                } catch (InterruptedException ex) {
+                    System.out.println("Soy el metodo run, algo paso");
+                }
+                System.out.println("El competidor: " + competidor.getNombre() + " Se ha movido, su pos actual es: " + competidor.getPosicion());
             }
-            System.out.println("El competidor: " + competidor.getNombre() + " Se ha movido, su pos actual es: " + competidor.getPosicion());
-        }
-        competidor.setTiempoDeLlegada(System.currentTimeMillis() - tiempoInicio);
-        System.out.println("El competidor: " + competidor.getNombre() + " Llego, su tiempo es: " + ((float) (competidor.getTiempoDeLlegada())/1000));
+            competidor.setTiempoDeLlegada(System.currentTimeMillis() - tiempoInicio);       
     }
-    
+
     public void accidente() {
-         try {
-                sleep(rng.nextInt(0, 250));
-            } catch (InterruptedException ex) {
-                System.out.println("Soy el metodo accidente, algo paso");
-            }
+        try {
+            sleep(rng.nextInt(0, 250));
+        } catch (InterruptedException ex) {
+            System.out.println("Soy el metodo accidente, algo paso");
+        }
     }
-    
+
     public void impulsar() {
         competidor.mover(5);
         cCarrera.moverCompetidorLabel(this, 5);
+    }
+    
+    public void pausarHilo() {
+        isPausado = true;
     }
 
     public Competidor getDatosCompetidor() {
--- src/main/java/edu/progavud/taller3/controller/ControlCarrera.java
+++ src/main/java/edu/progavud/taller3/controller/ControlCarrera.java
@@ -4,7 +4,9 @@
  */
 package edu.progavud.taller3.controller;
 
+import edu.progavud.taller3.model.Competidor;
 import java.util.ArrayList;
+import java.util.Random;
 import javax.swing.Timer;
 
 /**
@@ -13,38 +15,49 @@ import javax.swing.Timer;
  */
 public class ControlCarrera {
 
-    private ArrayList<CompetidorHilo> competidores;
+    private ArrayList<CompetidorHilo> competidoresHilos;
+    private ArrayList<Competidor> competidores;
     private ControlVentana cVentana;
     private int numeroCompetidores;
     private int distanciaCarrera;
 
     public ControlCarrera() {
+        competidoresHilos = new ArrayList<>();
         competidores = new ArrayList<>();
         cVentana = new ControlVentana(this);
         numeroCompetidores = cVentana.mostrarDigiteNumCompetidores() + 1;
         distanciaCarrera = 100;
         crearCompetidores();
-
     }
 
     public void crearCompetidores() { //este metodo se debe generalizar :3
-        CompetidorHilo competidorNuevo = new CompetidorHilo(this, "Usain Bolt");
+        Competidor competidorNuevo = new Competidor("Usain Bolt");
+
         competidores.add(competidorNuevo);
         for (int i = 1; i < numeroCompetidores; i++) {
-            competidorNuevo = new CompetidorHilo(this, cVentana.mostrarDigiteNomCompetidores());
+            competidorNuevo = new Competidor(cVentana.mostrarDigiteNomCompetidores());
             competidores.add(competidorNuevo);
         }
     }
 
+    public void inicializarHilos() {
+        competidoresHilos.clear();
+        for (Competidor competidor : competidores) {
+            CompetidorHilo competidorHiloNuevo = new CompetidorHilo(this, competidor);
+            competidoresHilos.add(competidorHiloNuevo);
+        }
+    }
+
     public void iniciarCarrera() {
-        for (CompetidorHilo competidor : competidores) {
+        inicializarHilos();
+        for (CompetidorHilo competidor : competidoresHilos) {
             competidor.start();
         }
         Timer timer = new Timer(100, null);
         timer.addActionListener(e -> {
             boolean todosTerminaron = true;
-            for (CompetidorHilo competidor : competidores) {
-                if (competidor.isAlive()) {
+            for (CompetidorHilo competidorHilo : competidoresHilos) {
+                if (competidorHilo.isAlive()) {
                     todosTerminaron = false;
                     break;
                 }
@@ -60,27 +73,27 @@ public class ControlCarrera {
     public String definirGanador() {
         String mensaje = null;
         ArrayList<Integer> indexGanadores = new ArrayList<>();
-        long mejorTiempo = competidores.get(0).getDatosCompetidor().getTiempoDeLlegada();
-        for (CompetidorHilo competidor : competidores) {
+        long mejorTiempo = competidoresHilos.get(0).getDatosCompetidor().getTiempoDeLlegada();
+        for (CompetidorHilo competidor : competidoresHilos) {
             if (competidor.getDatosCompetidor().getTiempoDeLlegada() < mejorTiempo) {
                 mejorTiempo = competidor.getDatosCompetidor().getTiempoDeLlegada();
                 indexGanadores.clear();
-                indexGanadores.add(competidores.indexOf(competidor));
+                indexGanadores.add(competidoresHilos.indexOf(competidor));
             } else if (competidor.getDatosCompetidor().getTiempoDeLlegada() == mejorTiempo) {
-                indexGanadores.add(competidores.indexOf(competidor));
+                indexGanadores.add(competidoresHilos.indexOf(competidor));
             }
         }
         if (indexGanadores.size() > 1) {
             mensaje = "Hubo un empate entre:\n";
             for (int index : indexGanadores) {
-                mensaje += competidores.get(index).getDatosCompetidor().getNombre() + "\n";
-                competidores.get(index).getDatosCompetidor().ganar();
+                mensaje += competidoresHilos.get(index).getDatosCompetidor().getNombre() + "\n";
+                competidoresHilos.get(index).getDatosCompetidor().ganar();
             }
-            mensaje += "Con un tiempo de: " + competidores.get(indexGanadores.get(0)).getDatosCompetidor().getTiempoDeLlegada();
+            mensaje += "Con un tiempo de: " + competidoresHilos.get(indexGanadores.get(0)).getDatosCompetidor().getTiempoDeLlegada();
         } else {
-            mensaje = "El ganador es: " + competidores.get(indexGanadores.get(0)).getDatosCompetidor().getNombre()
-                    + "\nCon un tiempo de: " + ((float) (competidores.get(indexGanadores.get(0)).getDatosCompetidor().getTiempoDeLlegada()) / 1000);
-            competidores.get(indexGanadores.get(0)).getDatosCompetidor().ganar();
+            mensaje = "El ganador es: " + competidoresHilos.get(indexGanadores.get(0)).getDatosCompetidor().getNombre()
+                    + "\nCon un tiempo de: " + ((float) (competidoresHilos.get(indexGanadores.get(0)).getDatosCompetidor().getTiempoDeLlegada()) / 1000);
+            competidoresHilos.get(indexGanadores.get(0)).getDatosCompetidor().ganar();
         }
         return mensaje;
     }
@@ -89,40 +102,44 @@ public class ControlCarrera {
         String mensajeVictoriasCompetidor = ""; //esta es la primera parte del mensaje :D
         ArrayList<Integer> indexGanadores = new ArrayList<>();
         int mayorNumVictorias = 0;
-        for (CompetidorHilo competidor : competidores) {
+        for (CompetidorHilo competidor : competidoresHilos) {
             mensajeVictoriasCompetidor += competidor.getDatosCompetidor().getNombre() + ", Victorias: " + competidor.getDatosCompetidor().getVictorias() + "\n";
             if (competidor.getDatosCompetidor().getVictorias() > mayorNumVictorias) {
                 mayorNumVictorias = competidor.getDatosCompetidor().getVictorias();
                 indexGanadores.clear();
-                indexGanadores.add(competidores.indexOf(competidor));
+                indexGanadores.add(competidoresHilos.indexOf(competidor));
             } else if (competidor.getDatosCompetidor().getTiempoDeLlegada() == mayorNumVictorias) {
-                indexGanadores.add(competidores.indexOf(competidor));
+                indexGanadores.add(competidoresHilos.indexOf(competidor));
             }
         }
         String mensajeGanadorAbsoluto = null;
         if (indexGanadores.size() > 1) {
             mensajeGanadorAbsoluto = "Hubo un empate entre:\n";
             for (int index : indexGanadores) {
-                mensajeGanadorAbsoluto += competidores.get(index).getDatosCompetidor().getNombre() + "\n";
+                mensajeGanadorAbsoluto += competidoresHilos.get(index).getDatosCompetidor().getNombre() + "\n";
             }
         } else {
-            mensajeGanadorAbsoluto = "El ganador Absoluto es: " + competidores.get(indexGanadores.get(0)).getDatosCompetidor().getNombre();
+            mensajeGanadorAbsoluto = "El ganador Absoluto es: " + competidoresHilos.get(indexGanadores.get(0)).getDatosCompetidor().getNombre();
         }
         return mensajeVictoriasCompetidor + "\n" + mensajeGanadorAbsoluto;
     }
 
     public void moverCompetidorLabel(CompetidorHilo competidor, int posAvanzada) {
-        cVentana.moverCompetidor(competidores.indexOf(competidor), posAvanzada, distanciaCarrera);
+        cVentana.moverCompetidor(competidoresHilos.indexOf(competidor), posAvanzada, distanciaCarrera);
     }
 
     public String accidenteCompetidor() {
-        competidores.get(0).accidente(); //Si en vez de 0, va un random de 0 hasta el size del array, se hace al azar para todos :D
-        return "El competidor 1  se accidento"; //tambien, con el random, esto es para el label que esta en el panel
+        Random rng = new Random();
+        int competidorAfectado = rng.nextInt(0, competidores.size());
+        competidoresHilos.get(competidorAfectado).accidente(); //Si en vez de 0, va un random de 0 hasta el size del array, se hace al azar para todos :D
+        return "El competidor " + (competidorAfectado + 1) +" se accidento"; //tambien, con el random, esto es para el label que esta en el panel
     }
 
     public String impulsarCompetidor() {
-        competidores.get(1).impulsar();
-        return "El competidor 2 se impulso";
+        Random rng = new Random();
+        int competidorAfectado = rng.nextInt(0, competidores.size());
+        competidoresHilos.get(competidorAfectado).impulsar();
+        return "El competidor: " + (competidorAfectado + 1) +"se impulso";
     }
 
     public int getDistanciaCarrera() {
@@ -138,9 +155,10 @@ public class ControlCarrera {
     }
 
     public void cerrarPrograma() {
-        for (CompetidorHilo competidor : competidores) {
+        
+        for (CompetidorHilo competidor : competidoresHilos) {
             if (competidor.isAlive()) {
-                competidor.stop();
+                competidor.pausarHilo();
             }
         }
         cVentana.mostrarMensaje(definirGanadorAbsoluto());
